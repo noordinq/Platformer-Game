416f9387e08fdddf0002764a0cb8cbc8cae4fbaa
noordinq <nooshqureish@gmail.com>
2024-05-21 11:09:17 +0300

    player class added

--- script.js
+++ script.js
@@ -12,3 +12,46 @@ let isCheckpointCollisionDetectionActive = true;
 const proportionalSize = (size) => {
   return innerHeight < 500 ? Math.ceil((size / 500) * innerHeight) : size;
 }
+
+class Player {
+    constructor() {
+      this.position = {
+        x: proportionalSize(10),
+        y: proportionalSize(400),
+      };
+      this.velocity = {
+        x: 0,
+        y: 0,
+      };
+      this.width = proportionalSize(40);
+      this.height = proportionalSize(40);
+    }
+    draw() {
+      ctx.fillStyle = "#99c9ff";
+      ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+    }
+    
+    update() {
+      this.draw();
+      this.position.x += this.velocity.x;
+      this.position.y += this.velocity.y;
+  
+      if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+        if (this.position.y < 0) {
+          this.position.y = 0;
+          this.velocity.y = gravity;
+        }
+        this.velocity.y += gravity;
+      } else {
+        this.velocity.y = 0;
+      }
+  
+      if (this.position.x < this.width) {
+        this.position.x = this.width;
+      }
+  
+      if (this.position.x >= canvas.width - 2 * this.width) {
+        this.position.x = canvas.width - 2 * this.width;
+      }
+    }
+}
